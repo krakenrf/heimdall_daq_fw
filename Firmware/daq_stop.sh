b97b461477701e37812af72509f31ff99ab27eca
krakenrf <78108016+krakenrf@users.noreply.github.com>
2021-12-28 16:30:42 +1300

    Update daq_stop.sh

--- Firmware/daq_stop.sh
+++ Firmware/daq_stop.sh
@@ -1,16 +1,14 @@
 #/bin/sh!
 echo "Shut down DAQ chain .."
-sudo kill $(ps aux | grep 'rtl' | awk '{print $2}')
-sudo killall -s 9 rtl*
+#sudo kill -64 $(ps aux | grep 'rtl' | awk '{print $2}')
+#sudo killall -s 9 rtl*
 
-sudo pkill rtl_daq.out
-sudo kill $(ps ax | grep "[p]ython3 _testing/test_data_synthesizer.py" | awk '{print $1}') 2> /dev/null
-sudo pkill sync.out
-sudo pkill decimate.out
-sudo pkill rebuffer.out
-sudo pkill squelch.out
-sudo kill $(ps ax | grep "[p]ython3 _daq_core/delay_sync.py" | awk '{print $1}') 2> /dev/null
-sudo kill $(ps ax | grep "[p]ython3 _daq_core/hw_controller.py" | awk '{print $1}') 2> /dev/null
-sudo pkill iq_server.out
-
-
+sudo pkill -64 rtl_daq.out
+sudo kill -64 $(ps ax | grep "[p]ython3 _testing/test_data_synthesizer.py" | awk '{print $1}') 2> /dev/null
+sudo pkill -64 sync.out
+sudo pkill -64 decimate.out
+sudo pkill -64 rebuffer.out
+sudo pkill -64 squelch.out
+sudo kill -64 $(ps ax | grep "[p]ython3 _daq_core/delay_sync.py" | awk '{print $1}') 2> /dev/null
+sudo kill -64 $(ps ax | grep "[p]ython3 _daq_core/hw_controller.py" | awk '{print $1}') 2> /dev/null
+sudo pkill -64 iq_server.out
